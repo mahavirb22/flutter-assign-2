776ec3ab358979d02ced51599e994ab6151069a5
mahavirb22 <bhandarimahavir2006@gmail.com>
2026-04-23 07:52:28 +0530

    Updation

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -118,7 +118,7 @@ class _RegisterPageState extends State<RegisterPage>{
       body: Center(
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            padding: const EdgeInsets.all(20.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -180,7 +180,7 @@ class _RegisterPageState extends State<RegisterPage>{
                 ElevatedButton(
                   onPressed: validate,
                   style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                    padding: EdgeInsets.all(10.0),
                   ),
                   child: Text('Register', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))
                 )
